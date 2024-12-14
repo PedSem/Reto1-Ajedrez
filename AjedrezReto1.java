eaeef5c5f5a1a06b808fd3ea5dbf7de31e30c782
Aidan_ <aidsaegar@alu.edu.gva.es>
2024-12-14 14:05:38 +0100

    Aidan 14/12/2024

--- AjedrezReto1.java
+++ AjedrezReto1.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class AjedrezReto1 {
     // Muestra la posicion de la pieza en el tablero.
-    public static void mostrarPosicion(char[][] tablero) {
+    public static void mostrarPosicion(int[][] tablero) {
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero[i].length; j++) {
                 if (tablero[j][i] == 1) {
-                    System.out.println("La posicion es " + (j + 1) + (i + 1));
+                    System.out.println("La posicion es " + columna(j + 1) + (i + 1));
                 }
             }
         }
@@ -15,14 +15,67 @@ public class AjedrezReto1 {
         Scanner sc = new Scanner(System.in);
         return sc.nextInt() - 1;
     }
+    public static String movimiento (int i, int letra, int numero) {
+        /*
+        | 1 2 3 |
+        | 4 X 5 |
+        | 7 8 9 |
+
+        X es la posicion del rey y los numero los posibles movimientos
+         */
+        switch (i) {
+            case 1:
+                return columna(letra - 1) + "" + (numero + 1) + ", ";
+            case 2:
+                return columna(letra) + "" + (numero + 1) + ", ";
+            case 3:
+                return columna(letra + 1) + "" + (numero + 1) + ", ";
+            case 4:
+                return columna(letra - 1) + "" + numero + ", ";
+            case 6:
+                return columna(letra + 1) + "" + numero + ", ";
+            case 7:
+                return columna(letra - 1) + "" + (numero - 1) + ", ";
+            case 8:
+                return columna(letra) + "" + (numero - 1) + ", ";
+            case 9:
+                return columna(letra + 1) + "" + (numero - 1);
+            default:
+                return "Error";
+        }
+    }
+    public static char columna(int i) {
+        return switch (i) {
+            case 1 -> 'a';
+            case 2 -> 'b';
+            case 3 -> 'c';
+            case 4 -> 'd';
+            case 5 -> 'e';
+            case 6 -> 'f';
+            case 7 -> 'g';
+            case 8 -> 'h';
+            default -> 'x';
+        };
+    }
     public static void main(String[] args) {
-        char[][] tablero = new char[8][8];
+        int[][] tablero = new int[8][8];
 
+        Scanner sc = new Scanner(System.in);
         System.out.print("Letra: ");
-        int letra = scanner(); // Esto es la coordenada de las letras
-        System.out.print("Numero: ");
+        String letraString = sc.nextLine(); // Esto es la coordenada de las letras
+        System.out.print("Número: ");
         int numero = scanner(); // Esto es la coordenada de los numero
-
+        int letra = switch (letraString) {
+            case "a" -> 1;
+            case "b" -> 2;
+            case "c" -> 3;
+            case "d" -> 4;
+            case "e" -> 5;
+            case "f" -> 6;
+            case "g" -> 7;
+            case "h" -> 8;
+            default -> 0;
+        } - 1;
 
         for (int i = tablero.length - 1; i >= 0; i--) {
             for (int j = 0; j < tablero.length; j++) {
@@ -30,40 +83,80 @@ public class AjedrezReto1 {
             }
         }
 
+        // Define la posicion del rey en el tablero con un 1
         tablero[letra][numero] = 1;
 
-        // Revertir Tablero
+        int c = 8; // Muestra las coordenadas de numeros por pantalla en el tablero
+
+        // Mostrar Tablero por pantalla
         for (int i = tablero.length - 1; i >= 0; i--) {
             for (int j = 0; j < tablero.length; j++) {
                 System.out.print(tablero[j][i] + " ");
             }
-            System.out.println();
+            System.out.println("| " + c);
+            c--;
         }
+        System.out.println("---------------");
+        System.out.println("a b c d e f g h"); // Muestra por pantalla las coordenadas de las letras en el tablero
+        System.out.println();
 
         mostrarPosicion(tablero);
 
+        String movimientos = "Posibles movimientos: ";
         letra += 1;
         numero += 1;
         if (letra != 1 && numero != 1 && letra != 8 && numero != 8) {
-            System.out.println("Esta dentro");
+            System.out.println("Está dentro");
 
-            String movimientos = "";
-            movimientos += ((letra - 1) + "" + numero + ", ");
-            movimientos += ((letra + 1) + "" + numero + ", ");
-            movimientos += (letra + "" + (numero + 1) + ", ");
-            movimientos += (letra + "" + (numero - 1) + ", ");
-            movimientos += ((letra - 1) + "" + (numero - 1) + ", ");
-            movimientos += ((letra - 1) + "" + (numero + 1) + ", ");
-            movimientos += ((letra + 1) + "" + (numero + 1) + ", ");
-            movimientos += ((letra + 1) + "" + (numero - 1));
+            // Posibles Movimientos del rey dentro de los bordes del tablero
+            movimientos += movimiento(1, letra, numero) + movimiento(2, letra, numero) + movimiento(3, letra, numero) + movimiento(4, letra, numero) + movimiento(6, letra, numero) + movimiento(7, letra, numero) + movimiento(8, letra, numero) + movimiento(9, letra, numero);
 
-            System.out.println(movimientos);
+        } else if (letra == 1 && numero == 1){
+            System.out.println("Esquina");
 
-            switch (letra) {
-                case 1:
+            // Posibles Movimientos del rey en la esquina inferior izquierda
+            movimientos += movimiento(2, letra, numero) + movimiento(3, letra, numero) + movimiento(6, letra, numero);
 
-            }
-        } else System.out.println("Fuera");
+        } else if (letra == 1 && numero == 8) {
+            System.out.println("Esquina");
 
+            // Posibles Movimientos del rey en la esquina superior izquierda
+            movimientos += movimiento(6, letra, numero) + movimiento(8, letra, numero) + movimiento(9, letra, numero);
+
+        } else if (letra == 8 && numero == 8) {
+            System.out.println("Esquina");
+
+            // Posibles Movimientos del rey en la esquina superior derecha
+            movimientos += movimiento(4, letra, numero) + movimiento(7, letra, numero) + movimiento(8, letra, numero);
+
+        } else if (letra == 8 && numero == 1) {
+            System.out.println("Esquina");
+
+            // Posibles Movimientos del rey en la esquina inferior derecha
+            movimientos += movimiento(1, letra, numero) + movimiento(2, letra, numero) + movimiento(4, letra, numero);
+
+        } else if (numero == 1) {
+            System.out.println("Borde");
+
+            // Posibles Movimientos del rey en el borde inferior (sin esquinas)
+            movimientos += movimiento(1, letra, numero) + movimiento(2, letra, numero) + movimiento(3, letra, numero) + movimiento(4, letra, numero) + movimiento(6, letra, numero);
+        } else if (letra == 1) {
+            System.out.println("Borde");
+
+            // Posibles Movimientos del rey en el borde izquierdo (sin esquinas)
+            movimientos += movimiento(2, letra, numero) + movimiento(3, letra, numero) + movimiento(6, letra, numero) + movimiento(8, letra, numero) + movimiento(9, letra,numero);
+        } else if (numero == 8) {
+            System.out.println("Borde");
+
+            // Posibles Movimientos del rey en el borde superior (sin esquinas)
+            movimientos += movimiento(4, letra, numero) + movimiento(6, letra, numero) + movimiento(7, letra, numero) + movimiento(8, letra, numero) + movimiento(9, letra, numero);
+        } else {
+            System.out.println("Borde");
+
+            // Posibles Movimientos del rey en el borde superior (sin esquinas)
+            movimientos += movimiento(1, letra, numero) + movimiento(2, letra, numero) + movimiento(4, letra, numero) + movimiento(7, letra, numero) + movimiento(8, letra, numero);
+        }
+
+        System.out.println(movimientos);
     }
 }
