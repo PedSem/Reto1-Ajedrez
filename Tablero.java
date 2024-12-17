f46fdcdebad0e084e1ddc39241776da180074523
aidan <aidsaegar@alu.edu.gva.es>
2024-12-17 07:36:11 +0100

    17/12/2024 Tablero y Rey Bien Modificados y con numeros

--- Tablero.java
+++ Tablero.java
@@ -1,27 +1,27 @@
 public class Tablero {
 
-    private String[][] tablero = new String[8][8];
+    private int[][] tablero = new int[8][8];
 
-    public void setTablero(String[][] tablero) {
+    public void setTablero(int[][] tablero) {
         this.tablero = tablero;
     }
 
-    public String[][] getTablero() {
-        for (int i = 0; i < tablero.length; i++){
-            for (int j = 0; j < tablero[i].length; j++) {
-                if ((i + j ) % 2 != 0){
-                    tablero[i][j] = "⬛ ";
-                } else {
-                    tablero[i][j] = "⬜ ";
-                }
+    public int[][] getTablero() {
+        int c = 8; // Muestra las coordenadas de numeros por pantalla en el tablero (derecha)
+
+        // Mostrar Tablero por pantalla
+        for (int i = tablero.length - 1; i >= 0; i--) {
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[j][i] + "  ");
             }
+            System.out.println("| " + c);
+            c--;
         }
-        for (int i = 0; i <tablero.length; i++){
-            for (int j = 0; j < tablero[i].length; j++){
-                System.out.print(tablero[i][j]);
-            }
-            System.out.println();
-        }
+
+        System.out.println("------------------------"); // Separador
+        System.out.println("a  b  c  d  e  f  g  h"); // Muestra por pantalla las coordenadas de las letras en el tablero.
+        System.out.println();
+
         return tablero;
     }
 }
