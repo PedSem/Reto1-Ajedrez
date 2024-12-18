22969b4c47f178bfc7b01042316cd7fe64522d4e
aidansaez <aidsaegar@alu.edu.gva.es>
2024-12-18 10:20:41 +0100

    Cambio de nombres
    y apunto de implementar en el Menu (Main)

---
+++ Rey.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Rey {
+    public static void Rey(String letraString, int numero) {
+        Tablero tb = new Tablero();
+        String[][] tablero;
+
+        // La letra que introduces es visual y hay que volver a ponerla como numero.
+        int letra = Tablero.LetraToNumero(letraString);
+
+        MovimientosRey movimientosRey = new MovimientosRey();
+
+        tablero = tb.InicializarTablero(); // Inicializa tablero en negro y blanco
+
+        String movimientos = (movimientosRey.Rey(tablero, letra, numero)); // Guarda los posibles movimientos en un String para mostrarlos después
+
+        tb.getTablero(); // Imprime el tablero modificado
+        System.out.println(movimientos); // Imprime los posibles movimientos
+    }
+}
